2bcaaabd304587d35eaacbb6ca418395bb9bfb1c
davidjumble <hellodavidparkinson@gmail.com>
2021-07-04 15:38:19 +0100

    adding status to the response

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.set('X-full-stack', '4life')
+  res.status(418)
+  res.send('sloppy   !')
 })
 
 app.listen(port, () => {
